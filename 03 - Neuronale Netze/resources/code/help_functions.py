72f5f5a5ba32c49239fd84265a06c4903603e85c
mipet23 <michailpetermann@web.de>
2023-03-19 20:52:49 +0100

    erste Version 03 - Neuronale Netze

--- 03 - Neuronale Netze/resources/code/help_functions.py
+++ 03 - Neuronale Netze/resources/code/help_functions.py
@@ -37,31 +37,59 @@ def pruefe_gewichte(nn, gewicht1, gewicht2, gewicht3, gewicht4):
       return result
     else:
       return "Super! Du hast alle Gewichte richtig abgelesen!"
-      
+
 def daten():
-    import random
-    import math	
+    import torch
+    from torch.autograd import Variable
+    n_data_train = torch.ones(200, 2)
+    n_data_test = torch.ones(50, 2)
+    x0 = torch.normal(n_data_train + torch.tensor([2.5,5]), 1)      # class0 x data (tensor), shape=(100, 2)
+    y0 = torch.zeros(200)               # class0 y data (tensor), shape=(100, 1)
+    x1 = torch.normal(n_data_train + torch.tensor([8,2]), 1)     # class1 x data (tensor), shape=(100, 2)
+    y1 = torch.ones(200)                # class1 y data (tensor), shape=(100, 1)
+    x_train = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating
+    y_train = torch.cat((y0, y1), ).type(torch.LongTensor)  
+    
+    n_data_test = torch.ones(50, 2)
+    x0 = torch.normal(n_data_test + torch.tensor([2.5,5]), 1)      # class0 x data (tensor), shape=(100, 2)
+    y0 = torch.zeros(50)               # class0 y data (tensor), shape=(100, 1)
+    x1 = torch.normal(n_data_test + + torch.tensor([8,2]), 1)     # class1 x data (tensor), shape=(100, 2)
+    y1 = torch.ones(50)                # class1 y data (tensor), shape=(100, 1)
+    x_test = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating
+    y_test = torch.cat((y0, y1), ).type(torch.LongTensor) 
+    
+    return Variable(x_train), Variable(y_train), Variable(x_test), Variable(y_test)
+  
+def datenpunkte_zeichnen(x_data, labels, farben):
     import matplotlib.pyplot as plt
-    daten = []
-    x_klasse_0 = []
-    y_klasse_0 = []
-    x_klasse_1 = []
-    y_klasse_1 = []
-    for _ in range(250):
-	    x = random.uniform(0,1) + 0
-	    y = random.uniform(0,1) + 2
-	    daten.append(((x,y), 0))
-	    x_klasse_0.append(x)
-	    y_klasse_0.append(y)
-	    
-    for _ in range(250):
-	    x = random.uniform(0,1) + 2
-	    y = random.uniform(0,1) + 0
-	    daten.append(((x,y), 1))
-	    x_klasse_1.append(x)
-	    y_klasse_1.append(y)
-	    
-    plt.scatter(x_klasse_0, y_klasse_0, color='#ec90cc')
-    plt.scatter(x_klasse_1, y_klasse_1, color='#4f7087')
-	  
-    return daten, plt
+    from matplotlib import colors
+    
+    plt.scatter(x_data.data.numpy()[:, 0], x_data.data.numpy()[:, 1], 
+            c=labels.data.numpy(), s=50, cmap=colors.ListedColormap(farben))
+    plt.show()
+    
+def daten2():
+    import torch
+    from torch.autograd import Variable
+    n_data_train = torch.ones(200, 2)
+    n_data_test = torch.ones(50, 2)
+    x0 = torch.normal(n_data_train + torch.tensor([6,5]), 1)      
+    y0 = torch.zeros(200)
+    x1 = torch.normal(n_data_train + torch.tensor([2,2]), 1)   
+    y1 = torch.ones(200)          
+    x2 = torch.normal(n_data_train + torch.tensor([10,2]), 1) 
+    y2 = 2 * torch.ones(200) 
+    x_train = torch.cat((x0, x1, x2), 0).type(torch.FloatTensor)
+    y_train = torch.cat((y0, y1, y2), ).type(torch.LongTensor)  
+    
+    n_data_test = torch.ones(50, 2)
+    x0 = torch.normal(n_data_test + torch.tensor([6,5]), 1)      
+    y0 = torch.zeros(50)
+    x1 = torch.normal(n_data_test + torch.tensor([2,2]), 1)   
+    y1 = torch.ones(50)          
+    x2 = torch.normal(n_data_test + torch.tensor([10,2]), 1) 
+    y2 = 2 * torch.ones(50) 
+    x_test = torch.cat((x0, x1, x2), 0).type(torch.FloatTensor) 
+    y_test = torch.cat((y0, y1, y2), ).type(torch.LongTensor) 
+    
+    return Variable(x_train), Variable(y_train), Variable(x_test), Variable(y_test)
