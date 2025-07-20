2686878ec48f32062e5524bd9f4b8753ddf6c909
Genius_G <Silas_G@mail.de>
2025-07-20 10:24:17 +0200

    bug fixing

--- 01 - Datenrepräsentation/01 - Datenrepräsentation.py
+++ 01 - Datenrepräsentation/01 - Datenrepräsentation.py
@@ -1,47 +1,47 @@
 import marimo
 
-__generated_with = "0.10.18"
+__generated_with = "0.13.15"
 app = marimo.App()
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
-        # Repräsentation von Informationen im Computer
-        
-        Nachdem du einige Grundlagen der Pythonprogrammierung kennen gelernt hast, widmen wir uns in diesem Abschnitt der Repräsentation von Informationen im Computer, die für die Anwendungen künstlicher Intelligenz besonders wichtig ist. 
-        
-        Um es möglichst einfach zu halten, betrachten wir zwei Arten von Gegenständen: die Eier von Blaumeisen und die Eier von Greifvögeln. 
-        
-        &nbsp;
-        
-        
-         <figure>
-          <img src="public/img/voegel_und_eier.png" alt="Vögel und Eier" style="width:45%">
-          <figcaption></figcaption>
-        </figure> 
-        
-        &nbsp;
-        
-        Diese Objekte unterscheiden sich z.B. in ihrer Helligkeit und Höhe voneinander. Die Eier der Blaumeisen sind relativ hell und klein. Demgegenüber sind die Eier mancher Greifvögel besonders hoch und relativ dunkel. Messen wir die Helligkeit und die Höhe eines Eis einer Blaumeise und eines Greifvogels, so könnte z.B. der zweidimensionale Punkt $p_1=(0.8, 2)$ ein Ei einer Blaumeise und $p_2=(0.3, 8)$ ein Ei eines Greifvogels beschreiben. Dabei steht der erste Wert für die Helligkeit und der zweite für die Höhe des Eis. Demzufolge können wir jedes Ei als Punkt in einem Koordinatensystem darstellen.  
-        
-        &nbsp;
-        
-        
-         <figure>
-          <img src="public/img/koordinatensystem_eier.png" alt="Koordinatensystem mit Eiern" style="width:45%">
-          <figcaption></figcaption>
-        </figure> 
-        
-        &nbsp;
-        
-        Für Computer sind also derartige Informationen nichts weiter als zwei-, drei- oder n-dimensionale Datenpunkte, denn ein Computer hat im Gegensatz zum Menschen kein intuitives Verständnis von Formen, Farben oder sonstigen Sinneswahrnehmungen. 
-        
-        ## Darstellung von Datenpunkten in Python
-        
-        Um Datenpunkte in Python darzustellen, gibt es unterschiedliche Bibliotheken, wie z.B. <i>NumPy</i> oder <i>PyTorch</i>. Für den jetzigen Anwendungsfall reicht es jedoch aus, einen Datenpunkt durch ein Zahlentupel zu beschreiben. Möchtest du diese Datenpunkte in einem zweidimensionalen Koordinatensystem abbilden, bietet dir die Bibliothek <i>Matplotlib</i> die passende Funktion dazu.
-        """
+    # Repräsentation von Informationen im Computer
+
+    Nachdem du einige Grundlagen der Pythonprogrammierung kennen gelernt hast, widmen wir uns in diesem Abschnitt der Repräsentation von Informationen im Computer, die für die Anwendungen künstlicher Intelligenz besonders wichtig ist. 
+
+    Um es möglichst einfach zu halten, betrachten wir zwei Arten von Gegenständen: die Eier von Blaumeisen und die Eier von Greifvögeln. 
+
+    &nbsp;
+
+
+     <figure>
+      <img src="public/img/voegel_und_eier.png" alt="Vögel und Eier" style="width:45%">
+      <figcaption></figcaption>
+    </figure> 
+
+    &nbsp;
+
+    Diese Objekte unterscheiden sich z.B. in ihrer Helligkeit und Höhe voneinander. Die Eier der Blaumeisen sind relativ hell und klein. Demgegenüber sind die Eier mancher Greifvögel besonders hoch und relativ dunkel. Messen wir die Helligkeit und die Höhe eines Eis einer Blaumeise und eines Greifvogels, so könnte z.B. der zweidimensionale Punkt $p_1=(0.8, 2)$ ein Ei einer Blaumeise und $p_2=(0.3, 8)$ ein Ei eines Greifvogels beschreiben. Dabei steht der erste Wert für die Helligkeit und der zweite für die Höhe des Eis. Demzufolge können wir jedes Ei als Punkt in einem Koordinatensystem darstellen.  
+
+    &nbsp;
+
+
+     <figure>
+      <img src="public/img/koordinatensystem_eier.png" alt="Koordinatensystem mit Eiern" style="width:45%">
+      <figcaption></figcaption>
+    </figure> 
+
+    &nbsp;
+
+    Für Computer sind also derartige Informationen nichts weiter als zwei-, drei- oder n-dimensionale Datenpunkte, denn ein Computer hat im Gegensatz zum Menschen kein intuitives Verständnis von Formen, Farben oder sonstigen Sinneswahrnehmungen. 
+
+    ## Darstellung von Datenpunkten in Python
+
+    Um Datenpunkte in Python darzustellen, gibt es unterschiedliche Bibliotheken, wie z.B. <i>NumPy</i> oder <i>PyTorch</i>. Für den jetzigen Anwendungsfall reicht es jedoch aus, einen Datenpunkt durch ein Zahlentupel zu beschreiben. Möchtest du diese Datenpunkte in einem zweidimensionalen Koordinatensystem abbilden, bietet dir die Bibliothek <i>Matplotlib</i> die passende Funktion dazu.
+    """
     )
     return
 
@@ -90,27 +90,17 @@ def _():
     # Koordinatensystem eintragen. Die Farbe der Punkte kannst du mit 
     # dem Parameter 'color' festlegen.
     plt.scatter([1], [1], color='red')
-    return (
-        d1,
-        d2,
-        d3,
-        datenpunkt,
-        datenpunkte,
-        plt,
-        warnings,
-        x_werte,
-        y_werte,
-    )
+    return (plt,)
 
 
 @app.cell
 def _(mo):
     mo.md(
         r"""
-        <img style="float: left;" src="public/img/laptop_icon.png" width=50 height=50 /> <br><br>
-        
-        <i>Plotte die Punkte des Graphen der Funktion $x^2$ im Koordinatensystem, deren x-Werte im Intervall von $-4$ bis $4$ liegen und jeweils den Abstand $0.5$ voneinander haben.</i>
-        """
+    <img style="float: left;" src="public/img/laptop_icon.png" width=50 height=50 /> <br><br>
+
+    <i>Plotte die Punkte des Graphen der Funktion $x^2$ im Koordinatensystem, deren x-Werte im Intervall von $-4$ bis $4$ liegen und jeweils den Abstand $0.5$ voneinander haben.</i>
+    """
     )
     return
 
@@ -121,28 +111,28 @@ def _():
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
-        ## k-Nearest-Neighbors (kNN)
-        
-        Im Folgenden schauen wir uns unseren ersten KI-Algorithmus an. Der k-Nearest-Neighbor-Algorithmus ist ein einfacher Klassifikationsalgorithmus. Im ersten Schritt wird dem Computer „mitgeteilt“, bei welchen Daten es sich um Datenpunkte der Klasse 0, 1, 2, etc. handelt; in unserem Fall welche Datenpunkte die Eier der Blaumeise und welche Datenpunkte die Eier von Greifvögeln repräsentieren. Im zweiten Schritt wird <i>k</i> festgelegt. Wir wählen $k=3$. Wenn wir nun einen neuen Datenpunkt klassifizieren müssen, dessen Klasse uns nicht bekannt ist, bestimmen wir die $k=3$ nächsten Nachbarn dieses Datenpunkts. Hat dieser Datenpunkt mehr Nachbarn, die als Eier der Blaumeise klassifiziert sind, wird auch dieser Datenpunkt als ein Ei der Blaumeise klassifiziert. Gibt es mehr Eier der Greifvögel als Nachbarn, wird der neue Datenpunkt als Ei eines Greifvogels klassiziert. Die Herausforderung des k-Nearest-Neighbours-Algorithmus besteht u.a. darin, <i>k</i> optimal zu wählen.
-        
-        &nbsp;
-        
-        
-         <figure>
-          <img src="public/img/koordinatensystem_eier_knn.png" alt="Koordinatensystem mit Eiern" style="width:45%">
-          <figcaption></figcaption>
-        </figure> 
-        
-        &nbsp;
-        
-        <img style="float: left;" src="public/img/laptop_icon.png" width=50 height=50 /> <br><br>
-        
-        <i> Jetzt betrachten wir Mini-Greifvögel, deren Eier etwas kleiner und heller sind als die der bisherigen Greifvögel. Außerdem haben wir ein paar unbekannte Eier (rot markierte Datenbpunkte), die entweder von den Mini-Greifvögeln oder den Blaumeisen stammen. Ordne die unbekannten Eier mit Hilfe des kNN-Algorithmus entweder der Klasse der Eier der Blaumeise oder der Eier der Greifvögel zu. Führe dazu das Feld aus, um alle Datenpunkte zu visualisieren. Implementiere anschließend den kNN-Algorithmus und führe die implementierte Funktion auf allen Datenpunkten aus. Ändert sich die Klassifikation für unterschiedliche <i>k</i>?</i>
-        """
+    ## k-Nearest-Neighbors (kNN)
+
+    Im Folgenden schauen wir uns unseren ersten KI-Algorithmus an. Der k-Nearest-Neighbor-Algorithmus ist ein einfacher Klassifikationsalgorithmus. Im ersten Schritt wird dem Computer „mitgeteilt“, bei welchen Daten es sich um Datenpunkte der Klasse 0, 1, 2, etc. handelt; in unserem Fall welche Datenpunkte die Eier der Blaumeise und welche Datenpunkte die Eier von Greifvögeln repräsentieren. Im zweiten Schritt wird <i>k</i> festgelegt. Wir wählen $k=3$. Wenn wir nun einen neuen Datenpunkt klassifizieren müssen, dessen Klasse uns nicht bekannt ist, bestimmen wir die $k=3$ nächsten Nachbarn dieses Datenpunkts. Hat dieser Datenpunkt mehr Nachbarn, die als Eier der Blaumeise klassifiziert sind, wird auch dieser Datenpunkt als ein Ei der Blaumeise klassifiziert. Gibt es mehr Eier der Greifvögel als Nachbarn, wird der neue Datenpunkt als Ei eines Greifvogels klassiziert. Die Herausforderung des k-Nearest-Neighbours-Algorithmus besteht u.a. darin, <i>k</i> optimal zu wählen.
+
+    &nbsp;
+
+
+     <figure>
+      <img src="public/img/koordinatensystem_eier_knn.png" alt="Koordinatensystem mit Eiern" style="width:45%">
+      <figcaption></figcaption>
+    </figure> 
+
+    &nbsp;
+
+    <img style="float: left;" src="public/img/laptop_icon.png" width=50 height=50 /> <br><br>
+
+    <i> Jetzt betrachten wir Mini-Greifvögel, deren Eier etwas kleiner und heller sind als die der bisherigen Greifvögel. Außerdem haben wir ein paar unbekannte Eier (rot markierte Datenbpunkte), die entweder von den Mini-Greifvögeln oder den Blaumeisen stammen. Ordne die unbekannten Eier mit Hilfe des kNN-Algorithmus entweder der Klasse der Eier der Blaumeise oder der Eier der Greifvögel zu. Führe dazu das Feld aus, um alle Datenpunkte zu visualisieren. Implementiere anschließend den kNN-Algorithmus und führe die implementierte Funktion auf allen Datenpunkten aus. Ändert sich die Klassifikation für unterschiedliche <i>k</i>?</i>
+    """
     )
     return
 
@@ -178,17 +168,7 @@ def _(plt):
     '''
     def knn(datenpunkte1, datenpunkte2, unbekannter_datenpunkt, k):
         pass
-    return (
-        farbe_eier_blaumeise,
-        farbe_eier_greifvogel,
-        helligkeit_eier_blaumeise,
-        helligkeit_eier_greifvogel,
-        helligkeit_eier_unbekannt,
-        hoehe_eier_blaumeise,
-        hoehe_eier_mini_greifvogel,
-        hoehe_eier_unbekannt,
-        knn,
-    )
+    return
 
 
 @app.cell
